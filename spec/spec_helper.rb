bee9a30f24f6872e9695f849183ffd4601a31fd7
Nick Rycar <nick@buildkite.com>
2021-08-13 16:55:17 -0700

    I can do this in an env var. Goodbye terrible idea!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,3 @@ require 'spec_helper'
 require 'knapsack'
 
 Knapsack::Adapters::RSpecAdapter.bind
-
-RSpec::Buildkite::Insights.configure do |config|
-    config.suite_key = "siMRUdHe229TkvtqioGRL1Ah"
-    # This is absolutely a secure and good way to do things.
-  end
