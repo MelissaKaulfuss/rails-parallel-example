a6d44915b71acf7f3b57e81e0045d29f016a3cbb
Nick Rycar <nick@buildkite.com>
2021-08-13 19:37:44 -0700

    make a web request for real?

--- spec/third_spec.rb
+++ spec/third_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
+require 'net/http'
 
 RSpec.describe "Third spec" do
   describe "GET" do
     it "returns 'Hello World'" do
-      get "https://google.com"
+      get Net::HTTP.get(URI.parse('http://www.google.com'))
+
 
       expect(response.status).to eql(200)
     end
