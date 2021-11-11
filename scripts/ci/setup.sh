135570454b77897d77ca76a8b82500d9798db41d
Nick Rycar <nick@buildkite.com>
2021-11-11 14:04:56 -0800

    Adding honeycomb config.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -2,24 +2,31 @@
 
 set -eu
 
-sudo yum install -y git gcc make readline-devel openssl-devel
-sudo yum install nodejs npm --enablerepo=epel
+# Install Steps
+
+STEP_START=$(date +%s)
+STEP_SPAN_ID=$(echo install | sum | cut -f 1 -d \ )
+
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-build-tools -- sudo yum install -y git gcc make readline-devel openssl-devel
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-npm -- sudo yum install nodejs npm --enablerepo=epel
 
 
-[ ! -d ~/.rbenv ] && git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-rbenv -- [ ! -d ~/.rbenv ] && git clone git://github.com/rbenv/rbenv.git ~/.rbenv
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # Install ruby-build system-widely
-[ ! -d /tmp/ruby-build ] && git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
-sudo /tmp/ruby-build/install.sh
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-rubybuild -- [ ! -d /tmp/ruby-build ] && git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build && sudo /tmp/ruby-build/install.sh
 
-rbenv install 2.4.1 && rbenv global 2.4.1
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-ruby -- rbenv install -l
 
-gem install bundle
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-bundler -- gem install bundle
 
-bundle install
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID bundle-install -- bundle install
 
-npm install
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID npm-install -- npm install
 
-bundle exec rake db:reset
+STEP_START=$(date +%s)
+STEP_SPAN_ID=$(echo raketasks | sum | cut -f 1 -d \ )
+
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID rake-db-reset -- bundle exec rake db:reset
