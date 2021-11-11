231f8cc5cb5d9f6ecf8b569684b5d3df7184e730
Nick Rycar <nick@buildkite.com>
2021-11-11 14:16:53 -0800

    Fix buildevents path

--- scripts/ci/parallel_specs.sh
+++ scripts/ci/parallel_specs.sh
@@ -5,7 +5,7 @@ set -eu
 STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo rspec | sum | cut -f 1 -d \ )
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID rspec-tests -- bundle exec rake knapsack:rspec
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID rspec-tests -- bundle exec rake knapsack:rspec
 
 
-./scripts/ci/buildevents build $BUILDKITE_BUILD_ID $BUILD_START success
+./buildevents build $BUILDKITE_BUILD_ID $BUILD_START success
--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -7,26 +7,26 @@ set -eu
 STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo install | sum | cut -f 1 -d \ )
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-build-tools -- sudo yum install -y git gcc make readline-devel openssl-devel
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-npm -- sudo yum install nodejs npm --enablerepo=epel
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-build-tools -- sudo yum install -y git gcc make readline-devel openssl-devel
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-npm -- sudo yum install nodejs npm --enablerepo=epel
 
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-rbenv -- [ ! -d ~/.rbenv ] && git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-rbenv -- [ ! -d ~/.rbenv ] && git clone git://github.com/rbenv/rbenv.git ~/.rbenv
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # Install ruby-build system-widely
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-rubybuild -- [ ! -d /tmp/ruby-build ] && git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build && sudo /tmp/ruby-build/install.sh
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-rubybuild -- [ ! -d /tmp/ruby-build ] && git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build && sudo /tmp/ruby-build/install.sh
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-ruby -- rbenv install -l
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-ruby -- rbenv install -l
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-bundler -- gem install bundle
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID install-bundler -- gem install bundle
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID bundle-install -- bundle install
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID bundle-install -- bundle install
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID npm-install -- npm install
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID npm-install -- npm install
 
 STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo raketasks | sum | cut -f 1 -d \ )
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID rake-db-reset -- bundle exec rake db:reset
+./buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID rake-db-reset -- bundle exec rake db:reset
