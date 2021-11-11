f64e8dbcf13588424802ecf60d7abe830f61e5b0
Nick Rycar <nick@buildkite.com>
2021-11-11 14:14:38 -0800

    Export build var

--- scripts/ci/parallel_specs.sh
+++ scripts/ci/parallel_specs.sh
@@ -2,4 +2,10 @@
 
 set -eu
 
-bundle exec rake knapsack:rspec
+STEP_START=$(date +%s)
+STEP_SPAN_ID=$(echo rspec | sum | cut -f 1 -d \ )
+
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID rspec-tests -- bundle exec rake knapsack:rspec
+
+
+./scripts/ci/buildevents build $BUILDKITE_BUILD_ID $BUILD_START success
