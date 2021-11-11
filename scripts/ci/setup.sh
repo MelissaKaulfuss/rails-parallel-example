ad9e57727d7ede85066a7db6c4476273da5edbb9
Nick Rycar <nick@buildkite.com>
2021-11-11 13:27:07 -0800

    Maybe don't change directories.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -10,8 +10,7 @@ eval "$(rbenv init -)"
 
 # Install ruby-build system-widely
 [ ! -d /tmp/ruby-build ] && git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
-cd /tmp/ruby-build
-sudo ./install.sh
+sudo /tmp/ruby-build/install.sh
 
 rbenv install 2.4.1 && rbenv global 2.4.1
 
