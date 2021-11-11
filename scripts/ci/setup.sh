557182e43a1a94271224e22a6eb09cd29ded5626
Nick Rycar <nick@buildkite.com>
2021-11-11 13:08:00 -0800

    I feel a little dirty doing this in the script. YOLO.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -2,7 +2,19 @@
 
 set -eu
 
-sudo yum -y install rbenv
+sudo yum install -y git gcc make readline-devel openssl-devel
+
+git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Install ruby-build system-widely
+git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
+cd /tmp/ruby-build
+sudo ./install.sh
+
+rbenv install 2.4.1 && rbenv global 2.4.1
 
 gem install bundle
 
