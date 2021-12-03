a8b2a66b10944d2c87d8b466994b236de84b6eb6
Mel Kaulfuss <melissacor3lfuss@gmail.com>
2021-12-03 12:17:19 +1100

    Moar specs

--- spec/second_spec.rb
+++ spec/second_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe "Second spec" do
+  it "totally should work" do
+    expect(1).to eql(1)
+  end
+
   it "totally works" do
-    expect(1).to eql(0)
+    expect(0).to eql(0)
   end
 end
